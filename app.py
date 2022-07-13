9f50bec72ac02adb41eb413e62494b3d8a5e6842
Oleg Borkhalenko <borkhalenko@gmail.com>
2022-07-13 23:17:30 +0300

    Just a save

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+"""
+TODO: write some smart at docstring
+"""
 import sys
 
 from PySide6.QtWidgets import QApplication
-from src.SacWindow import SacWindow
+from src.sac_window import SacWindow
 
 app = QApplication(sys.argv)
 
 window = SacWindow()
 
 window.show()
-  
 
-  
 sys.exit(app.exec())
