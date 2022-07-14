5637138b3bc9e59808d3905c591e9286e8151b6d
O.Borkhalenko <O.Borkhalenko@metal-holding.local>
2022-07-14 18:08:56 +0300

    Save...

--- src/sac_widget.py
+++ src/sac_widget.py
@@ -5,19 +5,46 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QVBoxLayout,
     QToolBar,
+    QTreeView,
+    QHeaderView,
+    QSizePolicy
 )
+
+from src.models.node_tree_model import NodeTreeModel
 
 
 class SacWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+        self.model = NodeTreeModel()
+        self.table_view = QTreeView()
+        self.table_view.setModel(self.model)
+
+        # # QTableView Headers
+        # self.horizontal_header = self.table_view.horizontalHeader()
+        # self.vertical_header = self.table_view.verticalHeader()
+        # self.horizontal_header.setSectionResizeMode(
+        #                        QHeaderView.ResizeToContents
+        #                        )
+        # self.vertical_header.setSectionResizeMode(
+        #                      QHeaderView.ResizeToContents
+        #                      )
+        #
+        # self.horizontal_header.setStretchLastSection(True)
+
+        size = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        size.setHorizontalStretch(1)
+        self.table_view.setSizePolicy(size)
+
         # creating layouts
-        main_layout = QVBoxLayout()
-        toolbar_layout = QHBoxLayout()
-        content_layout = QHBoxLayout()
-        main_layout.addLayout(toolbar_layout)
-        main_layout.addLayout(content_layout)
-        self.setLayout(main_layout)
+        self.main_layout = QVBoxLayout()
+        self.toolbar_layout = QHBoxLayout()
+        self.content_layout = QHBoxLayout()
+        self.main_layout.addLayout(self.toolbar_layout)
+        self.main_layout.addLayout(self.content_layout)
+
+        self.content_layout.addWidget(self.table_view)
+        self.setLayout(self.main_layout)
 
         # creating a toolbar in the toolbar layout
         #toolbar = QToolBar("Main toolbar")
